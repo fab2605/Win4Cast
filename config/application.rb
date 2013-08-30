fb93ee251f618fbae59aa1b3e5248c47894652b2
Fabien ANTHONIOZ <f.anthonioz@ceesto.org>
2013-08-30 20:39:27 +0200

    Modifie les generateurs de rails pour eviter de générer les fichiers js, css helper_spec et view_spec

--- config/application.rb
+++ config/application.rb
@@ -7,9 +7,19 @@ require "sprockets/railtie"
 Bundler.require(:default, Rails.env)
 
 module Win4Cast
-  class Application < Rails::Application
+	class Application < Rails::Application
+		config.generators do |g|
+			g.test_framework :rspec, fixture: true
+			g.fixture_replacement :factory_girl, dir: 'spec/factories'
+			g.view_specs false
+			g.helper_specs false
+			g.stylesheets = false
+			g.javascripts = false
+			g.helper = false
+		end
+	config.filter_parameters += [:password, :password_confirmation]
     # config.time_zone = 'Central Time (US & Canada)'
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-  end
+	end
 end
