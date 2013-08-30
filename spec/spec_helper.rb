06c409d19c3f8cbfd6488f3798f1bff0d3f9d094
Fabien ANTHONIOZ <f.anthonioz@ceesto.org>
2013-08-30 20:47:49 +0200

    Modifie spec_helper pour ajouter la config de email_spec et modifier celle de database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'email_spec'
 require 'rspec/autorun'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -11,7 +12,10 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
 
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
   require 'database_cleaner'
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.orm = "mongoid"
@@ -19,5 +23,6 @@ RSpec.configure do |config|
 
   config.before(:each) do
     DatabaseCleaner.clean
+    DatabaseCleaner.start
   end
 end
