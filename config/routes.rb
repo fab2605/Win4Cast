062a6459d405f45fa0304a29254883faa0369f5e
Fabien ANTHONIOZ <f.anthonioz@ceesto.org>
2013-08-30 20:40:50 +0200

    Modifie les routes et rajoute scope 'api'

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Win4Cast::Application.routes.draw do
-	root to: "home#index"
+	root :to => "home#index"
+
+	scope :api do
+
+		# # Sign in or out
+	 #    post "/sign_in", :to => "devise/sessions#create", as: "sign_in"
+	 #    delete "/sign_out", :to => "devise/sessions#destroy", as: "sign_out"
+		# post "/sign_up", to: "devise/registrations#create", as: "register"
+		# delete "/unregister", to: "devise/registrations#destroy", as: "unregister"
+
+	 #    # Password recovery
+		# post "/password", to: "devise/passwords#create", as: "create_password"
+		# put "/password", to: "devise/passwords#update", as: "update_password"
+
+		# # Profile update
+		# put "/edit_profile", to: "devise/registrations#update", as: "edit_profile"
+
+	end
 end
